2d8d236ed357242afa330aead53f57bfbb63494d
z-Wang30 <zijun.wang@nasa.gov>
2026-06-16 13:47:37 -0400

    Added set_val() and updated promotions; added optimizer (+ design var, objective, and constraint)

--- uqpce/examples/aircraft_design/run_example.py
+++ uqpce/examples/aircraft_design/run_example.py
@@ -1,57 +1,26 @@
 import openmdao.api as om
+from openmdao.utils.assert_utils import assert_check_partials
 
 #Import ExplicitComponents
-from aero import AeroDiscipline
+from aero import AeroDicipline
 from WeightsComp import Weights_Struct
 from propAndCost import Propulsion
 from propAndCost import EngineWeight
 from propAndCost import DOC
 from BreguetRangeComp import BreguetRangeComp
-
-class TotalMassComp(om.ExplicitComponent):
-    """
-    Component for "TotalMassComp" box containing analytical derivatives
-    """
-    def setup(self):
-        
-        #Local design variable
-        self.add_input('m_empty', desc='Empty mass')
-
-        #Parameter
-        self.add_input('m_payload', desc='Payload mass')
-
-        #Solver state
-        self.add_input('m_fuel', desc='Fuel mass')
-
-        #Output
-        self.add_output('m_total', desc='Total mass')
-
-    def setup_partials(self):
-        self.declare_partials('m_total', ['m_empty', 'm_payload', 'm_fuel'])
-
-    def compute(self, inputs, outputs):
-        m_empty = inputs['m_empty']
-        m_payload = inputs['m_payload']
-        m_fuel = inputs['m_fuel']
-
-        outputs['m_total'] = m_empty + m_fuel + m_payload
-    
-    def compute_partials(self, inputs, partials):
-        partials['m_total', 'm_empty'] = 1.0
-        partials['m_total', 'm_payload'] = 1.0
-        partials['m_total', 'm_fuel'] = 1.0
+from total_mass_comp import TotalMassComp
 
 class AeroStruct(om.Group):
     """
     Coupled solver loop with AeroComp, WeightsComp, TotalMassComp, and BreguetRangeComp
     """
     def setup(self):
-        self.add_subsystem('aero', AeroDiscipline(), 
-                           promotes_inputs= ['S', 'AR', 'V', 'rho', 'g', 'C_D0_base', 'ks_base', 'e_base'], 
-                           promotes_outputs= [('L/D', 'LD'), 'CL'])    
+        self.add_subsystem('aero', AeroDicipline(), 
+                           promotes_inputs= ['S', 'AR', 'V', 'rho', 'g', 'C_D0_base', 'ks_base', 'e_base', 'S_0', 'm_total'], 
+                           promotes_outputs= ['LD', 'CL'])    
     
         self.add_subsystem('mass', Weights_Struct(), 
-                           promotes_inputs= ['S', 'AR', 'V', 'm_engine', 'm_total'], 
+                           promotes_inputs= ['S', 'AR', 'V', 'm_engine', 'm_total', 'm_fuse', 'kw_base', 'fsys_base', 'p_base', 'V_ref'], 
                            promotes_outputs= ['m_empty'])
         
         self.add_subsystem('total_mass', TotalMassComp(), 
@@ -64,59 +33,100 @@ class AeroStruct(om.Group):
         
         #BalanceComp to determine converged value for 'm_fuel'
         bal = self.add_subsystem('range_bal', om.BalanceComp(), promotes=['*'])
-        bal.add_balance(name='m_fuel', eq_units='km', lhs_name='R_comp', rhs_name='R_target')
+        bal.add_balance(name='m_fuel', eq_units='m', lhs_name='R_comp', rhs_name='R_target', units='kg')
 
         #Setup solvers (linear + nonlinear)
-        self.nonlinear_solver = om.NewtonSolver(solve_subsystems=False)
+        self.nonlinear_solver = om.NewtonSolver(solve_subsystems=True)
         self.nonlinear_solver.options['iprint'] = 2
         self.nonlinear_solver.options['maxiter'] = 20
+        # self.nonlinear_solver.options['atol'] = 1e-6
+        # self.nonlinear_solver.options['rtol'] = 1e-9
         self.linear_solver = om.DirectSolver()
 
 #PROBLEM START
 p = om.Problem()
+
+#Add subsystems
 p.model.add_subsystem('prop', Propulsion(), 
                       promotes_inputs= ['SFC_tech', ('V_cruise', 'V'), 'SFC_ref', 'eta_base', 'kv_base', 'V_ref'], 
                       promotes_outputs= ['SFC'])
         
 p.model.add_subsystem('engine_weight', EngineWeight(), 
-                      promotes_inputs= ['SFC_tech', 'delta_alpha', 'm_eng_ref', 'alpha_base'], 
+                      promotes_inputs= ['SFC_tech', 'm_eng_ref', 'alpha_base'], 
                       promotes_outputs= ['m_engine'])
 
 p.model.add_subsystem('aerostruct', AeroStruct(), promotes=['*'])
 
 p.model.add_subsystem('DOC', DOC(),
-                      promotes_inputs=['SFC_tech', ('V_cruise', 'V'), ('R', 'R_comp'), 'm_fuel', 'Cf_base', 'C_time', 'k_acq', 'C_eng_ref', 'beta_base'],
-                      promotes_outputs=['DOC'])
+                      promotes_inputs=['SFC_tech', ('V_cruise', 'V'), ('R', 'R_comp'), 'm_fuel', 'Cf_base', 'C_time', 'k_acq', 'C_eng_ref', 'beta_base', 'N_pax'],
+                      promotes_outputs=['DOC', 'Dpm'])
 
-p.model.add_design_var('S', units='m**2')
-p.model.add_design_var('V', units='m/s')
-p.model.add_design_var('AR')
+#Setting model input defaults
+p.model.set_input_defaults('AR', val=9.45)
+p.model.set_input_defaults('V', units='m/s', val=230.)
+p.model.set_input_defaults('S', units='m**2', val=125.)
+#p.model.set_input_defaults('m_fuel', val=9000)
+
+#Setup optimizer
+p.driver = om.ScipyOptimizeDriver()
+p.driver.options['optimizer'] = 'SLSQP'
+# p.driver.options['tol'] = 1e-8
+
+#Set design variables, constraints, and objectives
+p.model.add_design_var('S', units='m**2', lower=0.)
+p.model.add_design_var('V', units='m/s', lower=0.)
+p.model.add_design_var('AR', lower=0.)
 p.model.add_design_var('SFC_tech', lower=-1.0, upper=1.0)
 
 p.model.add_constraint('CL', lower=0.0, upper=1.0) #CHANGE UPPER BOUND TO CL_MAX (STALL)
 
-p.model.add_objective('DOC') #Units?
+p.model.add_objective('Dpm')
 
-p.setup()
+p.model.approx_totals()
 
-#[p.set_val() here, or fixed.py file]
+#PROBLEM SETUP
+p.setup(force_alloc_complex=True)
 
-""""
-#Uncertain Variables
-self.set_input_defaults('delta_CD0', val=1.0)
-self.set_input_defaults('delta_ks', val=1.0)
-self.set_input_defaults('delta_e', val=1.0)
+#Set values of parameters, design variables, state variables, and constants
+p.set_val('R_target', 5.765e6, units='m')
+p.set_val('N_pax', 162.)
+p.set_val('m_payload', 16200., units='kg')
+p.set_val('SFC_ref', 1.6e-4, units='1/s')
+p.set_val('S_0', 124.6, units='m**2')
+p.set_val('V_ref', 235., units='m/s')
+p.set_val('m_fuse', 14000., units='kg')
+p.set_val('C_D0_base', 0.022)
+p.set_val('ks_base', 5.0e-5, units='1/m**2')
+p.set_val('e_base', 0.8)
+p.set_val('eta_base', 0.15)
+p.set_val('kv_base', 0.35)
+p.set_val('kw_base', 95.)
+p.set_val('fsys_base', 0.09)
+p.set_val('p_base', 1.0)
+p.set_val('alpha_base', 0.2)
+p.set_val('beta_base', 0.25)
+p.set_val('Cf_base', 0.8, units='USD/kg')
+p.set_val('m_eng_ref', 5200., units='kg')
+p.set_val('C_time', 2000., units='USD/h') # 2000 USD/hr
+p.set_val('k_acq', 1.)
+p.set_val('C_eng_ref', 2.0e7, units='USD')
 
-self.set_input_defaults('delta_kw', val=1.0)
-self.set_input_defaults('delta_fsys', val=1.0)
-self.set_input_defaults('delta_p', val=1.0)
+p.set_val('S', 125., units='m**2')
+p.set_val('AR', 9.45)
+p.set_val('V', 230., units='m/s')
+p.set_val('SFC_tech', 1.0)
 
-self.set_input_defaults('delta_eta', val=1.0)
-self.set_input_defaults('delta_kv', val=1.0)
-self.set_input_defaults('delta_alpha', val=1.0)
+p.set_val('m_fuel', 18000.)
 
-self.set_input_defaults('delta_Cf', val=1.0)
-self.set_input_defaults('delta_beta', val=1.0)
-"""
+p.set_val('rho', 1.225, units='kg/m**3') #.038
+p.set_val('g', 9.81, units='m/s**2')
 
-p.run_model()
+#PROBLEM SOLVE
+p.set_solver_print(level=0)
+p.run_driver()
+# om.n2(p)
+
+# partial_data = p.check_partials(out_stream=None, method='fd')
+# assert_check_partials(partial_data, atol=1e-12, rtol=1e-12)
+
+print(p.get_val('Dpm'))
